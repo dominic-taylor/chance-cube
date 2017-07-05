6ff59fd5c0059f540fc3ef6b9510b7215f26f18c
dominic-taylor <dataylorx@gmail.com>
2017-07-05 20:36:13 +1200

    Add nation data

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # chance-cube
 
-Randomly shows a UN member state, along with its capital and other relevant data.
+Randomly shows a nation state, along with its capital and other relevant data.
 I take no responsibilty for how accurate this is. 
 
 ## Data
-*Name
-*Languages
-*Population
+* Name (en)
+* Native Name
+* Capital
+* Languages
+* Population
+* Fun Facts
+
+
+## Todo
+* ~~Add nation data.~~ Filter data for only UN Member states. 
+* Stylin'
+* Custom settings for bg/text colour
+* Reload data on page click
+* Set timer to reload in development
+* Live data i.e current temperature, local time.
--- source.js
+++ source.js
@@ -1,26 +1,31 @@
 
-var nations = [{name: "New Zealand", capital: "Wellington", pop: "4.5m (2016)", facts:["i live here", "never rains"]},
-               {name: "Iran", capital: "Tehran", pop: "4.5m (2016)", facts:["great", "polite"]},
-               {name: "France", capital: "Paris", pop: "4.5m (2016)", facts:["80 m"]},
-               {name: "Portugal", capital: "Lisbon", pop: "4.5m (2016)", facts:["All drugs decriminlasied"]},
-               {name: "USA", capital: "Washington, DC", pop: "300m (2016)", facts:["fucked"]},
-               {name: "Spain", capital: "Madrid", pop: "80m (2016)", facts:["meah", "never rains"]},
-               {name: "China", capital: "Beijing", pop: "1b (2016)", facts:["good", "rains"]},
-               {name: "Brazil", capital: "Brasila", pop: "220m (2016)", facts:["Language: Portguesuse", "Pop: heaps"]},
-               {name: "Afghanistan", capital: "Kabul", pop: "35m (2016)", facts:["Language: Pashto, Dari", "Pop: 32m"]}
+// var nations = [{name: "New Zealand", capital: "Wellington", pop: "4.5m (2016)", facts:["i live here", "never rains"]},
+//                {name: "Iran", capital: "Tehran", pop: "4.5m (2016)", facts:["great", "polite"]},
+//                {name: "France", capital: "Paris", pop: "4.5m (2016)", facts:["80 m"]},
+//                {name: "Portugal", capital: "Lisbon", pop: "4.5m (2016)", facts:["All drugs decriminlasied"]},
+//                {name: "USA", capital: "Washington, DC", pop: "300m (2016)", facts:["fucked"]},
+//                {name: "Spain", capital: "Madrid", pop: "80m (2016)", facts:["meah", "never rains"]},
+//                {name: "China", capital: "Beijing", pop: "1b (2016)", facts:["good", "rains"]},
+//                {name: "Brazil", capital: "Brasila", pop: "220m (2016)", facts:["Language: Portguesuse", "Pop: heaps"]},
+//                {name: "Afghanistan", capital: "Kabul", pop: "35m (2016)", facts:["Language: Pashto, Dari", "Pop: 32m"]}
 
-]
+// ]
 
 //{name: "New Zealand", capital: "Wellington", pop: '4.5m (2016)', facts:["i live here", "never rains"]},
 
+console.log(data[0].name)
+console.log(data.length)
+
+//window.onload = function() {
+
+var nations = data
 var randomNumber = Math.floor(Math.random()*(nations.length));
 
 
-window.onload = function() {
 
   document.getElementById('heading').innerHTML = nations[randomNumber].name
   document.getElementById('sub').innerHTML= nations[randomNumber].capital 
-  document.getElementById('pop').innerHTML= nations[randomNumber].pop
+  document.getElementById('pop').innerHTML= nations[randomNumber].population
 
   var factList = ""
   for (var i = 0; i < nations[randomNumber].facts.length; i++) {
@@ -28,7 +33,7 @@ window.onload = function() {
   }
   document.getElementById('facts').innerHTML = factList
 
-}
+//}
 
 //Uncomment for testing
 // set timer to reload/call onload function to test working. 
